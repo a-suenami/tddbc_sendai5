1e37222f09e1802429d83bdc07d93d3722d476be
Akira SUENAMI <a.suenami@gmail.com>
2015-11-15 11:38:04 +0900

    Do refactoring isNeighborOf() method.

--- grid_point.go
+++ grid_point.go
@@ -14,8 +14,21 @@ func (gridPoint GridPoint) getNotation() string {
 }
 
 func (gridPoint GridPoint) isNeighborOf(other GridPoint) bool {
-    return (gridPoint.X == other.X     && gridPoint.Y == other.Y - 1) ||
-           (gridPoint.X == other.X - 1 && gridPoint.Y == other.Y    ) ||
-           (gridPoint.X == other.X     && gridPoint.Y == other.Y + 1) ||
-           (gridPoint.X == other.X + 1 && gridPoint.Y == other.Y    )
+    return gridPoint.isTopOf(other) ||
+           gridPoint.isBottomOf(other) ||
+           gridPoint.isRightOf(other) ||
+           gridPoint.isLeftOf(other)
+}
+
+func (gridPoint GridPoint) isTopOf(other GridPoint) bool {
+    return gridPoint.X == other.X && gridPoint.Y == other.Y + 1
+}
+func (gridPoint GridPoint) isBottomOf(other GridPoint) bool {
+    return gridPoint.X == other.X && gridPoint.Y == other.Y - 1
+}
+func (gridPoint GridPoint) isRightOf(other GridPoint) bool {
+    return gridPoint.X == other.X + 1 && gridPoint.Y == other.Y
+}
+func (gridPoint GridPoint) isLeftOf(other GridPoint) bool {
+    return gridPoint.X == other.X - 1 && gridPoint.Y == other.Y
 }
