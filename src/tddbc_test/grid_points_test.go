03c5f60dc6e5ba48c9bc41ada16a0b241d96eb3d
Akira SUENAMI <a.suenami@gmail.com>
2015-11-15 12:52:07 +0900

    Add test case of Contains() method and do refactoring it.

--- src/tddbc_test/grid_points_test.go
+++ src/tddbc_test/grid_points_test.go
@@ -5,11 +5,25 @@ import (
     . "tddbc"
 )
 
-func Test_格子点の集合(t *testing.T) {
+type gridPointsContainsTestParameter struct {
+    gridPoints GridPoints
+    target GridPoint
+    result bool
+}
+
+func Test_格子点の集合に指定した格子点が含まれているか判定できる(t *testing.T) {
     gridPoints := NewGridPoints( GridPoint{ X: 3, Y: 7 }, GridPoint{ X: 4, Y: 8 } )
 
-    target := GridPoint{ X: 3, Y: 7 }
-    if gridPoints.Contains(target) == false {
-        t.Fail()
+    gridPointsContainsTestParameters := []gridPointsContainsTestParameter{
+        gridPointsContainsTestParameter{ gridPoints: gridPoints, target: GridPoint{ X: 3, Y: 7 }, result: true },
+        gridPointsContainsTestParameter{ gridPoints: gridPoints, target: GridPoint{ X: 4, Y: 8 }, result: true },
+        gridPointsContainsTestParameter{ gridPoints: gridPoints, target: GridPoint{ X: 1, Y: 2 }, result: false } }
+
+    for _, parameter := range gridPointsContainsTestParameters {
+        actual := parameter.gridPoints.Contains(parameter.target)
+        expected := parameter.result
+        if expected != actual {
+            t.Errorf("expected: %v, actual: %v", expected, actual)
+        }
     }
 }
